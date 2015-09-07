85b349570330c430c2d6416549bab24e5f3beecf
marb <marb61a@gmail.com>
2015-09-07 13:54:05 +0000

    added custom css and editing articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,43 @@
 class ArticlesController < ApplicationController
+  
   def index
+    @articles = Article.all
   end
+  
+  def new
+    @article = Article.new
+  end
+  
+  def create
+    
+  end
+  
+  def edit
+    
+  end
+  
+  def update
+    
+  end
+  
+  def show
+    
+  end
+  
+  def destroy
+    
+  end
+  
+  
+  
+  private
+    def article_params
+      
+    end
+    
+    def set_article
+      
+    end
+  
+  
 end
