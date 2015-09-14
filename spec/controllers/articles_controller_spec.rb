8c4812ad929f2d324cb5a6118cd1de4be298f80c
marb <marb61a@gmail.com>
2015-09-14 11:15:32 +0000

    updating controller specs

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -1,8 +1,21 @@
 require 'rails_helper'
+require 'support/macros'
 
 RSpec.describe ArticlesController, type: :controller do
 
-  describe "GET #index" do
+  describe "GET #edit" do
+    before do
+      
+    end
+    
+    context "owner is allowed to edit his articles" do
+      
+    end
+    
+    context "non-owner is not allowed to edit other users articles" do
+      
+    end
+    
     it "returns http success" do
       get :index
       expect(response).to have_http_status(:success)
