95e4ea926a110dfa38abba5b9e9c17e732909abd
marb <marb61a@gmail.com>
2015-09-27 15:37:58 +0000

    finished updating spec tests

--- spec/features/signing_users_in_spec.rb
+++ spec/features/signing_users_in_spec.rb
@@ -2,15 +2,15 @@ require "rails_helper"
 
 RSpec.feature "Users sign-in" do
     before do
-        
+        @joe = User.create!(email: "joe@example.com", password: "password")
     end
     
     scenario "with valid credentials" do
         visit "/"
         
         click_link "Sign in"
-        fill_in "Email"
-        fill_in "Password"
+        fill_in "Email", with: @joe.email
+        fill_in "Password", with: @joe.password
         click_button "Log in"
         
         expect(page).to have_content("Signed in successfully.")
--- spec/features/signing_users_out_spec.rb
+++ spec/features/signing_users_out_spec.rb
@@ -2,11 +2,12 @@ require "rails_helper"
 
 RSpec.feature "Signing out signed-in users" do
     before do
+        @joe = User.create!(email: "joe@example.com", password: "password")
         visit "/"
     
         click_link "Sign in"
-        fill_in "Email"
-        fill_in "Password"
+        fill_in "Email", with: @joe.email
+        fill_in "Password", with: @joe.password
         click_button "Log in"
     end
     
